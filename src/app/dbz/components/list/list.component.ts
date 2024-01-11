67707e9c99e26244d0f16de84d4e891f946f813b
Zenon <zenropru@gmail.com>
2024-01-11 11:47:06 +0100

    Clase 70

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-dbz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
 
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter()
+
+  onDeleteCharacter(index: number):void {
+    this.onDelete.emit(index);
+  }
 }
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
+import { CommonModule } from '@angular/common';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
   exports: [
     MainPageComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
   ]
 })
 export class DbzModule { }
